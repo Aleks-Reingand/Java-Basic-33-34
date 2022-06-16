8a2e566326818e700e8d1f0bb54e440152236c25
Aleks R <aleks.r@siltatel.fi>
2022-06-16 14:20:28 +0300

    Initial commit

---
+++ src/main/java/com/telran/berlin/prictice/Dog.java
@@ -0,0 +1,16 @@
+package com.telran.berlin.prictice;
+
+public class Dog {
+    String breed;
+    int age;
+    String name;
+
+    public void setBreed(String text) {
+        breed = text;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+}
