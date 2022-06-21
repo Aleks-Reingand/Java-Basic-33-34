84ed269e19a95bd4f31a94c3400d7b1d39a25975
Aleks R <aleks.r@siltatel.fi>
2022-06-21 10:05:47 +0300

    introduction

---
+++ src/main/java/com/telran/berlin/homework/introduction/Arithmetic.java
@@ -0,0 +1,8 @@
+package com.telran.berlin.homework.introduction;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        System.out.println((55 + 9) % 9);
+    }
+
+}
---
+++ src/main/java/com/telran/berlin/homework/introduction/Cat.java
@@ -0,0 +1,67 @@
+package com.telran.berlin.homework.introduction;
+
+public class Cat {
+    int size;
+    int age;
+    String name;
+    String breed;
+    boolean isFluffy;
+    String color;
+    double weight;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public boolean isFluffy() {
+        return isFluffy;
+    }
+
+    public void setFluffy(boolean fluffy) {
+        isFluffy = fluffy;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
